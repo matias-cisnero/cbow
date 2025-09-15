67547d916e8cc799d69bb76eaaf9a7ea92497cdc
matias-cisnero <matiascisnero2014@gmail.com>
2025-09-15 17:36:00 -0300

    Agregado el algoritmo CBOW con cupy

--- cbow cupy.py
+++ cbow cupy.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cupy as cp
 
 with open("corpus.txt", "r", encoding="utf-8") as f:
     corpus = f.read().splitlines()
@@ -12,13 +13,16 @@ word_to_idx = {word: i for i, word in enumerate(vocab)}
 idx_to_word = {i: word for word, i in enumerate(vocab)}
 
 def softmax(h):
-    num = np.exp(h)
-    return num / np.sum(num)
+    num = cp.exp(h)
+    return num / cp.sum(num)
 
 def entrenar_cbow(corpus, vocab_size, word_to_idx, nombre_pc, epocas=1, η=0.001, N=300, C=4, W1=None, W2=None):
     if W1 == None or W2 == None:
-        W1 = np.random.normal(0, 0.1, (vocab_size, N))
-        W2 = np.random.normal(0, 0.1, (N, vocab_size))
+        W1 = cp.random.normal(0, 0.1, (vocab_size, N))
+        W2 = cp.random.normal(0, 0.1, (N, vocab_size))
+    else:
+        W1 = cp.asarray(W1)
+        W2 = cp.asarray(W2)
 
     print(f"Comienzo de entrenamiento con {epocas} epocas.")
     for epoca in range(epocas):
@@ -34,7 +38,7 @@ def entrenar_cbow(corpus, vocab_size, word_to_idx, nombre_pc, epocas=1, η=0.001
 
             # ---Propagación---
 
-            h = np.mean(W1[palabras_contexto_indices], axis=0).reshape(-1, 1)
+            h = cp.mean(W1[palabras_contexto_indices], axis=0).reshape(-1, 1)
 
             u = W2.T @ h
 
@@ -42,7 +46,7 @@ def entrenar_cbow(corpus, vocab_size, word_to_idx, nombre_pc, epocas=1, η=0.001
 
             # ---Retropropagación---
 
-            E = -u[palabra_central_indice] + np.log(np.sum(np.exp(u)))
+            E = -u[palabra_central_indice] + cp.log(cp.sum(cp.exp(u)))
 
             e = y.copy()
             e[palabra_central_indice] -= 1
@@ -53,14 +57,18 @@ def entrenar_cbow(corpus, vocab_size, word_to_idx, nombre_pc, epocas=1, η=0.001
 
             W1[palabras_contexto_indices] -= η * (1/C) * EH.T
 
-            print(f"Época {epoca}, Palabra: {i}/{len(corpus)}, Error: {E.item():.4f}")
+            if i % 1000 == 0:
+                print(f"Época {epoca}, Palabra: {i}/{len(corpus)}, Error: {E.item():.4f}")
 
         print(f"Fin de época: {epoca}")
 
         # ---Guardado de Pesos---
-        nombre_archivo = f'pesos_cbow_{nombre_pc}_epoca{epoca}.npz'
-        np.savez(nombre_archivo, W1=W1, W2=W2, eta=η, N=N, C=C)
-        print(f"Pesos e hiperparámetros guardados exitosamente en '{nombre_archivo}'")
+        if epoca % 50 == 0:
+            nombre_archivo = f'pesos_cbow_{nombre_pc}_epoca{epoca}.npz'
+            W1_np = cp.asnumpy(W1)
+            W2_np = cp.asnumpy(W2)
+            np.savez(nombre_archivo, W1=W1_np, W2=W2_np, eta=η, N=N, C=C)
+            print(f"Pesos e hiperparámetros guardados exitosamente en '{nombre_archivo}'")
 
     print(f"Entrenamiento con {epocas} terminado.")
     return W1, W2
@@ -73,28 +81,22 @@ def cargar_modelo_completo(nombre_archivo='pesos_cbow_pc2_epoca0.npz'):
     try:
         data = np.load(nombre_archivo)
         
-        # Carga de los pesos (arreglos)
         W1 = data['W1']
         W2 = data['W2']
-        
-        # --- Carga de los hiperparámetros (escalares) ---
-        # Usamos .item() para convertirlos de array(valor) a valor
+    
         N = data['N'].item()
         C = data['C'].item()
         eta = data['eta'].item()
         
-        print(f"Modelo cargado exitosamente desde '{nombre_archivo}'")
-        print(f"  Shape de W1: {W1.shape}")
-        print(f"  Shape de W2: {W2.shape}")
-        print("  Hiperparámetros recuperados ⚙️:")
-        print(f"    N (Embedding Dim) = {N}")
-        print(f"    C (Context Size) = {C}")
-        print(f"    η (Learning Rate) = {eta}")
-        
+        print()
+
         return W1, W2, N, C, eta
         
     except FileNotFoundError:
         print(f"Error: No se encontró el archivo '{nombre_archivo}'.")
         return None, None, None, None, None
 
-W1, W2 = entrenar_cbow(corpus, vocab_size, word_to_idx, "pc2", 500)
+W1, W2 = entrenar_cbow(corpus, vocab_size, word_to_idx, "pc2", 500, 0.01, 100)
+
+#W1, W2, N, C, eta = cargar_modelo_completo()
+#print(f"Shape de W: {W1.shape}, Shape de W':{W2.shape}, Tipo: {type(W1)}")
---
+++ pruebas_cupy.py
@@ -0,0 +1,4 @@
+import cupy as cp
+
+x = cp.array([1, 2, 3, 4, 5])
+x.device
